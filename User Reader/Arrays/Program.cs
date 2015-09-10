c462fa4ca61e6731bd7e4bda53f53e277b81edfc
Michael B. <mbilenko03@gmail.com>
2015-09-09 19:32:07 -0500

    Added Some Array Stuff || I Sometimes Use It

---
+++ User Reader/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      //Declaring Size:
+
+      int size;
+      int[] arrVariable = new datatype[size];
+
+      //Accessing Elements:
+
+      int[] arrVariable = {List of Values Ex: 1,2,3,4,5};
+      datatype variable = arrVariable[Which Element Ex: 0];
+
+      //Iterating Array:
+
+      for (int i=0; i < oldNumbers.Length; i++)
+      {
+        int number = oddNumbers[i]
+      }
+
+    }
+  }
+}
