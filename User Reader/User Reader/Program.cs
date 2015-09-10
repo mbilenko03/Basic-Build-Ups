e07b8e84cfbc7d4631e46c96f930f80c160f3e94
Michael B. <mbilenko03@gmail.com>
2015-09-09 19:03:24 -0500

    One of the main things I use in all my programs...

---
+++ User Reader/User Reader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Reader
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter what term you want: ");
+      string readTerm = Console.ReadLine();
+      int useTerm = int.Parse(readTerm);
+    }
+  }
+}
