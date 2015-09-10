a90e90ce0e27b06e3c7432055a51a6a16596792f
Michael B. <mbilenko03@gmail.com>
2015-09-09 20:08:41 -0500

    Added Exceptions Portion!

---
+++ User Reader/Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        //code from which you are trying to catch an exception
+      }
+      catch (Exception variable)
+      {
+        //code to do if the exception is present
+        //throw; lets the method who called the current method to deal with the exception.
+      }
+      finally
+      {
+        //code that always runs
+      }
+
+      
+    }
+  }
+}
