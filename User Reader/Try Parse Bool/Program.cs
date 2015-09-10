9d4e470094769abddc86589191ff14a67a9a1fca
Michael B. <mbilenko03@gmail.com>
2015-09-09 19:09:03 -0500

    Added Try Pars Bool!

---
+++ User Reader/Try Parse Bool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Parse_Bool
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool result = double.TryParse(userNumber, out number);
+      if (result == true)
+      {
+      }
+      else
+      {
+      }
+    }
+  }
+}
